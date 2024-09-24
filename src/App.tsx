069110f10db0f204b3d6562897098549397baa19
Aidan Isbell <IOLV@Outlook.com>
2024-09-24 13:47:04 +0100

    Initial setup for input file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css'
+import { useState } from 'react'
+import IngredientsInput from './components/ingredient-input/ingredient-input';
+
+// const [response, setResponse] = useState(""); // State for chat response
+// State for user input
 
 function App() {
-
+  const [userInput, setUserInput] = useState("Hi there!");
   return (
     <>
-      <div>Hello World</div>
+      <div>Koalas rule</div>
+      <IngredientsInput userInput={userInput} setUserInput={setUserInput} />
     </>
   )
 }
