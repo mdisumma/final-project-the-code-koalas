493663713d180148994ce05532e78dc41fb57e8a
Marco Di Summa <mdisumma@yahoo.com>
2024-09-27 09:49:16 +0100

    fix-leaf

--- src/Components/Leaf/leaf.tsx
+++ src/Components/Leaf/leaf.tsx
@@ -1,38 +1,40 @@
-import React from 'react';
-import LeafLeft from '../../../Leaf_Left.png';
-import LeafRight from '../../../Leaf_Right.png';
+import React from "react";
+import LeafLeft from "../../../Leaf_Left.png";
+import LeafRight from "../../../Leaf_Right.png";
 
 const Leaf: React.FC = () => {
-    return (
-      <>
-        <img
-          src={LeafRight}
-          alt="Leaf Right"
-          style={{
-            position: 'fixed',
-            right: 0,
-            top: 20,
-            width: '30%',
-            height: 'auto',
-            transformOrigin: 'center',
-            animation: 'sway 5s ease-in-out infinite',
-          }}
-        />
-        <img
-          src={LeafLeft}
-          alt="Leaf Left"
-          style={{
-            position: 'fixed',
-            left: 0,
-            top: 20,
-            width: '30%',
-            height: 'auto',
-            transformOrigin: 'center',
-            animation: 'sway 5s ease-in-out infinite',
-          }}
-        />
-      </>
-    );
-  };
-  
-  export default Leaf;
+  return (
+    <>
+      <img
+        src={LeafRight}
+        alt="Leaf Right"
+        style={{
+          position: "fixed",
+          right: 0,
+          top: 20,
+          width: "30%",
+          height: "auto",
+          transformOrigin: "center",
+          animation: "sway 5s ease-in-out infinite",
+          zIndex: -1,
+        }}
+      />
+      <img
+        src={LeafLeft}
+        alt="Leaf Left"
+        style={{
+          position: "fixed",
+          left: 0,
+          top: 20,
+          width: "30%",
+          height: "auto",
+          transformOrigin: "center",
+          animation: "sway 5s ease-in-out infinite",
+          zIndex: -1,
+        }}
+      />
+    </>
+  );
+};
+
+export default Leaf;
